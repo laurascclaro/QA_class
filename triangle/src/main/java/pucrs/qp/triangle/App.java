5c9ca5073fb1ab84e8cb3513b193c17c9e44d65f
LAURA SCHMITZ CAMPOS CLARO <17104931@pucrs.br>
2019-09-09 22:20:33 -0300

    second class incompleted

--- triangle/src/main/java/pucrs/qp/triangle/App.java
+++ triangle/src/main/java/pucrs/qp/triangle/App.java
@@ -4,10 +4,20 @@ package pucrs.qp.triangle;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static int identificaTriangulo(int a, int b, int c) {
+		if ((a < b + c) && (b < a + c) && (c < b + a)) {
+			if ((a == b) && (b == c))
+				return tipos.EQUILATERO.value();
+			else if ((a != b) && (a != c) && (b != c))
+				return tipos.ESCALENO.value();
+			else
+				return tipos.ISOSCELES.value();
+		}
+		return tipos.INVALIDO.value();
+	}
+
+	public static void main(String[] args) {
+		System.out.println(identificaTriangulo (1,3,2));
+	}
 }
